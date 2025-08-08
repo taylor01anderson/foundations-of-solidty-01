95d0fa0625c788de28721c04650d373ab5e1dfa9
taylor01anderson <taylor01anderson@icloud.com>
2025-08-07 22:00:04 -0700

    array practice

--- solidity-exercises-01.sol
+++ solidity-exercises-01.sol
@@ -1,8 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-// This contract is practicing arrays, mappings, and structures in Solidity.
+// These contracts are for practicing constructors, arrays, and mappings in Solidity.
 
+// Contracts to understand constructors 
 contract Member {
     // Initialize name and age upon depolyment
     string name;
@@ -40,7 +41,6 @@ contract teacherVariable is Member {
 
 // Constructor exercise 01:
 // Contract that stores fully accessible (inside and outside) integer data upon deployment
-
 contract Base {
     uint numberOne;
 
@@ -50,10 +50,68 @@ contract Base {
 }
 
 // Contract that derives data from the above the Base contract that runs a function and outputs the data to 5
-
 contract Derived is Base (5) {
 
     function get5() public view returns(uint) {
         return numberOne; 
     }
 }
+
+// Array practice
+contract learnArrays {
+    uint[] public myArray1;
+    uint[] public myArray2;
+    uint[200] public myFixedSizedArray;
+
+    // push() adds element to end of the array and returns the length
+    function push(uint number) public {
+        myArray1.push(number);
+    }
+
+    // pop() method removes the last element of an array and returns that value
+    function pop() public {
+        myArray1.pop();
+    }
+
+    // length() is a string property that determines the length of a string
+    function getLength() public view returns(uint) {
+        return myArray1.length;
+    }
+
+    // delete changes the value in an array to 0
+    function remove(uint _i) public {
+        delete myArray1[_i];
+    }
+
+    // Exercise to completely remove an element from an array rather than use delete and set it to 0
+    uint[] public changeArray;
+
+    // remove element removes the specified element and replaces it with the last element
+    function removeElement(uint i) public {
+        // set i to last element in the array
+        changeArray[i] = changeArray[changeArray.length -1];
+        // remove last element from array
+        changeArray.pop();
+    }
+
+    // Create a function that adds the values 1, 2, 3, and 4 to the array
+    function test() public {
+        changeArray.push(1);
+        changeArray.push(2);
+        changeArray.push(3);
+        changeArray.push(4);
+    }
+
+    // Or use a for loop
+    function testForLoop() public {
+        for(uint i=1; i <=4; i++) {
+            changeArray.push(i);
+        }
+    }
+
+    // Get the Values in changeArray    
+    function getChangeArray() public view returns(uint[] memory) {
+        return changeArray;
+    }
+}
+
