459d5a58fb68c0773a88bd396f21091a67658cd7
taylor01anderson <taylor01anderson@icloud.com>
2025-08-07 20:53:21 -0700

    constructor practice with hardcode and variable parameters

--- solidity-exercises-01.sol
+++ solidity-exercises-01.sol
@@ -13,3 +13,27 @@ contract Member {
         age = _age;
     }
 }
+
+// Contract inheriting from member contract with hardcoded variables
+contract Teacher is Member {
+
+    // constructor for teacher that sets the name and age
+    constructor() Member('Rachel', 28) {}
+
+    // function to return the name intantiated in the constructor
+    function getName() public view returns(string memory) {
+        return name;
+    }
+}
+
+// Contract inheriting from member contract with variable inputs
+contract teacherVariable is Member {
+
+    // constructor for teacher that sets the name and age
+    constructor(string memory inputName, uint inputAge) Member(inputName, inputAge) {}
+
+    // function to return the name intantiated in the constructor
+    function getName() public view returns(string memory) {
+        return name;
+    }
+}
